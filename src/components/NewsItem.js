62a5cf8788cfcbe77b7f952cc0eba0567083bcc2
ParthDesaiiiiii <118049894+ParthDesaiiiiii@users.noreply.github.com>
2025-07-22 14:51:40 -0400

    Used state for component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/3cbd/live/5ee79350-4b7f-11f0-86d5-3b52b53af158.jpg" className="card-img-top" alt="..."/>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetails/" className="btn btn-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
