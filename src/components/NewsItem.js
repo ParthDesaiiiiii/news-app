a222399837dee7f77aec2dd66296b1c3aac2d96f
ParthDesaiiiiii <118049894+ParthDesaiiiiii@users.noreply.github.com>
2025-07-29 14:07:18 -0400

    Fetched API from newsAPI to ppulate news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2025/04/Pixel-9-Pro-XL-with-collection.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
         </div>
         </div>
