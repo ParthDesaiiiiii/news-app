ee80ff8e9b46ed0d7dfab7833f674ebbd34bf034
ParthDesaiiiiii <118049894+ParthDesaiiiiii@users.noreply.github.com>
2025-09-09 12:50:14 -0400

    Addded features for titles and learned component lifecycle

--- src/components/News.js
+++ src/components/News.js
@@ -15,14 +15,19 @@ export class News extends Component {
     category: PropTypes.string
   }
 
-  constructor(){
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props){
+    super(props);
     // console.log("This is a constructor from news component");
     this.state = {
       articles: [],
       loading: false,
       page: 1
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - DailyNews`;
   }
 
   async updateNews(){
@@ -51,7 +56,7 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1 className="text-center" style={{margin: '35px 0px'}}>DailyNews - Top Headlines</h1>   
+        <h1 className="text-center" style={{margin: '35px 0px'}}>DailyNews - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>   
         {this.state.loading && <Spinner/>}    
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
